9b0b1ae060d3fc2bc821812e08cff6f9674bc210
Saehful Amri <amrishf97@gmail.com>
2023-05-03 17:13:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Table from "react-bootstrap/Table";
+// import List from "./List";
+import { useState, useEffect } from "react";
+import { uid } from "uid";
+import axios from "axios";
+
+let api = axios.create({ baseURL: "http://localhost:3000" });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div
+                className="fixed-top bg-white pb-3 mx-auto"
+                style={{ width: 600 }}>
+                <h1 className="px-3 d-flex justify-content-center py-3">
+                    My Contact List
+                </h1>
+                <Form>
+                    <Form.Group
+                        className="mb-3 form-group"
+                        controlId="formBasicEmail">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            className="form-control"
+                            type="text"
+                            placeholder="Enter a name"
+                        />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>No. Telp</Form.Label>
+                        <Form.Control
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter a number"
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+                <div style={{ marginTop: 50 }}>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Nama</th>
+                                <th>No Telp.</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                    </Table>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const List = () => {
+    return (
+        <div key={} className="list-group-item list-group-item-action">
+            <div className="row">
+                <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1"></h5>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default List;
